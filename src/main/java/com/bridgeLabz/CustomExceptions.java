85e195d559cabcfac138ee0fbcbf7a9da66e6b00
Bharath-SR <bharath.s0941@gmail.com>
2022-10-07 07:40:50 +0530

    UC2 : Added UC2 null pointer exception

---
+++ src/main/java/com/bridgeLabz/CustomExceptions.java
@@ -0,0 +1,10 @@
+package com.bridgeLabz;
+
+public class CustomExceptions extends Exception{
+
+    //user defined exceptions
+
+    CustomExceptions(String string){
+        super(string);
+    }
+}
--- src/main/java/com/bridgeLabz/MoodAnalyzer.java
+++ src/main/java/com/bridgeLabz/MoodAnalyzer.java
@@ -4,23 +4,38 @@ public class MoodAnalyzer {
 
     String message;
 
-    public MoodAnalyzer() {
-    }
-
-    public MoodAnalyzer(String message) {
-        this.message = message;
-
-    }
-
-    public String analyseMood() {
-        try {
-            if (this.message.contains("sad")) {
-                return "sad";
-            } else {
-                return "happy";
-            }
-        } catch (NullPointerException e) {
-            return "happy";
+//    public MoodAnalyzer() {
+//    }
+//
+//    public MoodAnalyzer(String message) {
+//        this.message = message;
+//
+//    }
+//
+//    public String analyseMood() {
+//        try {
+//            if (this.message.contains("sad")) {
+//                return "sad";
+//            } else {
+//                return "happy";
+//            }
+//        } catch (NullPointerException e) {
+//            return "Entered value is null";
+//        }
+//    }
+public static void main(String[] args) {
+    try{
+       String message=null;
+        if(message==null){
+            throw new CustomExceptions("Given String is null");
         }
+        else if(message.contains("Happy")){
+            System.out.println("Happy");
+        }else{
+            System.out.println("Sad");
+        }
+    }catch(CustomExceptions ex){
+        System.out.println(ex);
     }
+}
 }
